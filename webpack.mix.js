b47a9a6804f8631609bbec216c56fd66be420a67
landing-russia <landing-russia@yandex.ru>
2021-02-04 14:01:50 +0300

    webpack.mix.js options add

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,7 @@ const mix = require('laravel-mix');
 mix.js('src/js/app.js', 'public/js')
     .postCss('src/css/app.css', 'public/css', [
         require('tailwindcss')('./tailwind.config.js')
-    ]);
+    ])
+    .options({
+        processCssUrls: false
+    });
